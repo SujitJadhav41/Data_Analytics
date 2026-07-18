8a74305a1b8458de9932c023bdaaa1720b6fb594
SUJIT MAHADEV JADHAV <sujitmahadevjadhav@SUJITs-MacBook-Air.local>
2026-07-18 12:46:33 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+name = input("What is your name?: ")
+work = input("Where do you work: ")
+live = input("Where do you live?: ")
+
+# Harry works at CodeWithHarry and lives in Delhi
+# print(name + " works at " + work + " and lives in " + live) 👎
+
+print(f"{name} works at {work} and lives in {live}")
